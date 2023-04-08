3b59113306c160baca2a4db4c7734320270eefd6
Badansi <dchancerelle@yahoo.fr>
2023-04-08 23:23:07 +0200

    modification et ajout de la phase2

--- firstscrap.py
+++ firstscrap.py
@@ -1,16 +1,18 @@
-#importe de la bibliothèque CSV
 import requests
 from bs4 import BeautifulSoup
 import csv
 
-# Récupérer la page web du livre à recuperer
+def get_soup(url):
+    # Récupérer le contenu de la page web et parser avec BeautifulSoup
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup
+
+# Récupérer les données du livre spécifique
 url = "http://books.toscrape.com/catalogue/the-dirty-little-secrets-of-getting-your-dream-job_994/index.html"
-response = requests.get(url)
+soup = get_soup(url)
 
-# Parser(annalyse et extraction à partir du html) le contenu de la page web avec BeautifulSoup
-soup = BeautifulSoup(response.text, "html.parser")
-
-# Extraire les données specifiques par ligne(ce quil faut récuperer)
+# Extraire les données spécifiques à partir de la page web
 product_page_url = url
 upc = soup.select_one("table tr:nth-of-type(1) td").text
 title = soup.select_one("h1").text
@@ -27,3 +29,27 @@ with open("books_data.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["product_page_url", "universal_product_code (upc)", "title", "price_including_tax", "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image_url"])
     writer.writerow([product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url])
+
+# Récupérer les URL de la catégorie "Art"
+url = "http://books.toscrape.com/catalogue/category/books/art_25/index.html"
+soup = get_soup(url)
+
+# Trouver tous les liens de produit sur la page et extraire l'URL de chaque produit
+product_links = soup.select("h3 a")
+product_urls = [link["href"] for link in product_links]
+
+# Parcourir les pages de pagination pour récupérer toutes les URL de produit
+next_page = soup.select_one("li.next a")
+while next_page:
+    next_page_url = next_page["href"]
+    soup = get_soup(next_page_url)
+    product_links = soup.select("h3 a")
+    product_urls += [link["href"] for link in product_links]
+    next_page = soup.select_one("li.next a")
+
+# Écrire les URL de produit dans un fichier CSV
+with open("art_books_urls.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["product_url"])
+    for url in product_urls:
+        writer.writerow([url])
